e828e04f65f96c2752647e930a66172e73196daa
Eddie Chapman <eddie.chapman4@gmail.com>
2021-10-02 14:36:26 -0500

    Clean URL data

--- data-raw/clean.R
+++ data-raw/clean.R
@@ -7,7 +7,7 @@ library(stringr)
 reddit_path <- file.path('data-raw', 'reddit_data.csv')
 docs_path <- file.path('data-raw', 'docs.csv')
 quotes_path <- file.path('data-raw', 'quotes.csv')
-
+urls_path <- file.path('data-raw', 'extracted_urls.csv')
 
 reddit_df <- 
   readr::read_csv(file = reddit_path) %>%
@@ -58,6 +58,12 @@ codes_df <-
   mutate(code = stringr::str_split(codes, ' ')) %>%
   tidyr::unnest_longer(codes) %>%
   select(quote_id, doc_id, code)
+
+
+urls_df <-
+  readr::read_csv(file = urls_path) %>%
+  mutate(url_id = row_number()) %>%
+  select(url_id, document_id, url, platform, profile, content, community)
   
   
 # output
@@ -65,6 +71,7 @@ write_csv(reddit_df, file = file.path('data', 'reddit.csv'))
 write_csv(quotes_df, file = file.path('data', 'quotes.csv'))
 write_csv(docs_df, file = file.path('data', 'docs.csv'))
 write_csv(codes_df, file = file.path('data', 'codes.csv'))
+write_csv(urls_df, file = file.path('data', 'urls.csv'))
 
 
   
