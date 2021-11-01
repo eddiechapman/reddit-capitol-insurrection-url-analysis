03d3c663ffd41c78dadbc7dfae99afb2a9359f88
eddiechapman <eddie.chapman4@gmail.com>
2021-11-01 16:50:58 -0500

    Fix plots, move to plot directory

--- analyses/reddit_archive_sources.R
+++ analyses/reddit_archive_sources.R
@@ -5,7 +5,7 @@ reddit_archive_sources_path <- file.path('data', 'reddit_archive_sources.csv')
 
 reddit_archive_df <- readr::read_csv(file = reddit_archive_sources_path)
 
-reddit_archive_df %>%
+p <- reddit_archive_df %>%
   mutate(source = forcats::fct_reorder(source, size_gb)) %>%
   ggplot(aes(x = source, y = size_gb)) +
   geom_bar(stat = 'identity', fill = "#f68060", alpha = .6 , width = .4) +
@@ -16,9 +16,19 @@ reddit_archive_df %>%
     title = '"Archiving the Capitol Hill Riots" Sources',
     caption = 'Source: https://mega.nz/folder/30MlkQib#RDOaGzmtFEHkxSYBaJSzVA') +
   theme_bw()
+
+p
+
+ggsave(
+  filename = 'reddit_archive_sources.png',
+  path = file.path('plots'),
+  plot = last_plot(), 
+  scale = 1, 
+  dpi = 300
+)
   
 
-reddit_archive_df %>%
+p <- reddit_archive_df %>%
   mutate(source = forcats::fct_reorder(source, items)) %>%
   ggplot(aes(x = source, y = items)) +
   geom_bar(stat = 'identity', fill = "#f68060", alpha = .6 , width = .4) +
@@ -30,7 +40,17 @@ reddit_archive_df %>%
     caption = 'Source: https://mega.nz/folder/30MlkQib#RDOaGzmtFEHkxSYBaJSzVA') +
   theme_bw()
 
-reddit_archive_df %>%
+p
+
+ggsave(
+  filename = 'reddit_archive_sources_count.png',
+  path = file.path('plots'),
+  plot = last_plot(), 
+  scale = 1, 
+  dpi = 300
+)
+
+p <- reddit_archive_df %>%
   ggplot(aes(x = size_gb, y = items)) +
   geom_jitter(width = 2, size = 3, aes(col = source)) +
   ggrepel::geom_text_repel(aes(label = source), size = 4) +
@@ -39,3 +59,13 @@ reddit_archive_df %>%
   labs(title = '"Archiving the Captiol Hil Riots" Source Size and Volume',
        caption = 'Source: https://mega.nz/folder/30MlkQib#RDOaGzmtFEHkxSYBaJSzVA') +
   theme_bw()
+
+p
+
+ggsave(
+  filename = 'reddit_archive_sources_size_and_count.png',
+  path = file.path('plots'),
+  plot = last_plot(), 
+  scale = 1, 
+  dpi = 300
+)
